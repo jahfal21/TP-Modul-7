b2332cf8204a770502e802c039c4b38af2b9c80d
jahfal21 <jahfalramadhan4@gmail.com>
2022-04-08 17:16:21 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul7_1302204034/CovidConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using System.Diagnostics;
+
+namespace tpmodul7_1302204034
+{
+    public class CovidConfig
+    {
+        public string CONFIG1 { get; set; } = "celcius";
+        public int CONFIG2 { get; set; } = 14;
+        public string CONFIG3 { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        public string CONFIG4 { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+        public string satuan_suhu { get; set; }
+        public int batas_hari_demam { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+
+        public void ReadJSON()
+        {
+            string jsonText = File.ReadAllText("covid_config.json");
+            CovidConfig json = JsonSerializer.Deserialize<CovidConfig>(jsonText);
+            Debug.Assert(json != null);
+
+            this.satuan_suhu = json.satuan_suhu;
+            this.batas_hari_demam = json.batas_hari_demam;
+            this.pesan_ditolak = json.pesan_ditolak;
+            this.pesan_diterima = json.pesan_diterima;
+        }
+
+        public void UbahSatuan()
+        {
+            if (CONFIG1 == "celcius") { CONFIG1 = "farenhait"; }
+            else { CONFIG1 = "celcius"; };
+        }
+    }
+}
--- tpmodul7_1302204034/Program.cs
+++ tpmodul7_1302204034/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace tpmodul7_1302204034
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            CovidConfig config = new CovidConfig();
+            config.ReadJSON();
+
+            Console.WriteLine($"Berapakah suhu badan anda saat ini ? Dalam nilai {config.CONFIG1}");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman ?");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            bool kondisi1;
+            if (config.CONFIG1 == "celcius")
+            {
+                kondisi1 = (input1 >= 36.5) && (input1 <= 37.5);
+            }
+            else
+            {
+                kondisi1 = (input1 >= 97.7) && (input1 <= 99.5);
+            }
+
+            if (kondisi1 && input2 < config.CONFIG2)
+            {
+                Console.WriteLine(config.CONFIG4);
+            }
+            else
+            {
+                Console.WriteLine(config.CONFIG3);
+            }
+
+            Console.WriteLine("\nTest ubah config1");
+            Console.WriteLine(config.CONFIG1);
+            config.UbahSatuan();
+            Console.WriteLine(config.CONFIG1);
         }
     }
 }
